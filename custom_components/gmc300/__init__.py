140195d19f5406cdbc53a25627a0362dfad084b7
Sergey <rpmadm@yandex.ru>
2022-09-06 20:52:25 +0300

    Update __init__.py

--- custom_components/gmc300/__init__.py
+++ custom_components/gmc300/__init__.py
@@ -1,1 +1,9 @@
 DOMAIN = "gmc300"
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import Config, HomeAssistant
+from homeassistant.exceptions import ConfigEntryNotReady
+
+async def async_setup(hass: HomeAssistant, config: Config):
+    """Set up this integration using YAML is not supported."""
+    return True
