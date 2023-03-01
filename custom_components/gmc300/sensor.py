b134c1a28f31a7c6936c4f1d5e2f129ee6c0a09e
Sergey <rpmadm@yandex.ru>
2023-03-01 17:47:45 +0300

    Update

--- custom_components/gmc300/sensor.py
+++ custom_components/gmc300/sensor.py
@@ -1,4 +1,3 @@
-
 from __future__ import annotations
 
 from homeassistant.components.sensor import (
@@ -11,28 +10,21 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 
-import logging
 
-import serial
-
-_LOGGER = logging.getLogger(__name__)
-
-
-async def async_setup_platform(
+def setup_platform(
     hass: HomeAssistant,
     config: ConfigType,
-    async_add_entities: AddEntitiesCallback,
+    add_entities: AddEntitiesCallback,
     discovery_info: DiscoveryInfoType | None = None
 ) -> None:
-    async_add_entities(
-        [RadiationSensor(hass)], True)
+    """Set up the sensor platform."""
+    add_entities([ExampleSensor()])
 
 
-    
-class RadiationSensor(SensorEntity):
+class ExampleSensor(SensorEntity):
     """Representation of a Sensor."""
 
-    _attr_name = "Radiation Level"
+    _attr_name = "Example Temperature"
     _attr_native_unit_of_measurement = TEMP_CELSIUS
     _attr_device_class = SensorDeviceClass.TEMPERATURE
     _attr_state_class = SensorStateClass.MEASUREMENT
@@ -42,118 +34,3 @@ class RadiationSensor(SensorEntity):
         This is the only method that should fetch new data for Home Assistant.
         """
         self._attr_native_value = 23
-
-
-
-
-
-
-
-
-#import aiohttp
-#import voluptuous as vol
-#import homeassistant.helpers.config_validation as cv
-#from homeassistant.components.sensor import PLATFORM_SCHEMA
-
-#from homeassistant.core import HomeAssistant
-#from homeassistant.helpers.entity import Entity
-#from homeassistant.helpers.entity_platform import AddEntitiesCallback
-#from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
-
-#CONF_NAME = 'name'
-#CONF_START = 'start'
-#CONF_DESTINATION = 'destination'
-
-#ICON = 'mdi:Radioactive'
-
-#CONF_USB = 'usb_port'
-
-#PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-#    vol.Required(CONF_USB): cv.string
-#})
-
-#_LOGGER = logging.getLogger(__name__)
-
-#async def async_setup_platform(
-#        hass, config, async_add_entities, discovery_info=None):  # pylint: disable=unused-argument
-    """Setup sensor platform."""
-#    name = config['name']
-#    usb_port: config['usb_port']
-
-#    async_add_entities(
-#        [RadiationSensor(hass, name, usb_port)], True)
-
-
-
-class RadiationSensor(Entity):
-    """GMC-300 Sensor class"""
-
-    def __init__(self, hass, name, usb_port):
-        self.hass = hass
-        self._state = None
-        self._name = name
-        self._usb_port = usb_port
-        self.attr = {}
-        _LOGGER.debug('Initialized sensor %s with %s, %s', self._name, self._start, self._destination)
-
-    async def async_update(self):
-        """Update sensor."""
-        _LOGGER.debug('%s - Running update', self._name)
-
-        try:
-            rdn_device = serial.Serial( "/dev/ttyUSB-geiger", 57600 )
-
-            _LOGGER.debug('Requesting url %s', url)
-
-        except Exception as error:  # pylint: disable=broad-except
-            _LOGGER.debug('%s - Could not update - %s', self._name, error)
-
-#    @classmethod
-#    def is_coord(cls, data: str) -> bool:
-#        return bool(coords_re.fullmatch(data))
-
-    @property
-    def usb_port(self):
-        return self._usb_port
-
-#    @property
-#    def destination(self):
-#        return self.point_to_coords(self._destination)
-
-#    def point_to_coords(self, point: str) -> str:
-#        if YandexMapsSensor.is_coord(point):
-#            return point
-
-#        state = self.hass.states.get(point)
-#        if state:
-#            latitude = state.attributes.get('latitude')
-#            longitude = state.attributes.get('longitude')
-#            if latitude and longitude:
-#                return "{},{}".format(longitude, latitude)
-#            else:
-#                raise AttributeError
-
-    @property
-    def name(self):
-        """Name."""
-        return self._name
-
-    @property
-    def state(self):
-        """State."""
-        return self._state
-
-    @property
-    def icon(self):
-        """Icon."""
-        return ICON
-
-    @property
-    def unit_of_measurement(self):
-        """unit_of_measurement."""
-        return 'мин'
-
-    @property
-    def extra_state_attributes(self):
-        """Attributes."""
-        return self.attr
