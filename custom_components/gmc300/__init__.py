f38729aa056b4f90fc797b439e92685aafa92aef
Sergey <rpmadm@yandex.ru>
2023-03-10 21:33:44 +0300

    Update

--- custom_components/gmc300/__init__.py
+++ custom_components/gmc300/__init__.py
@@ -3,3 +3,22 @@
 
     # Return boolean to indicate that initialization was successful.
 #    return True
+import logging
+from .const import *
+from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import *
+import homeassistant.helpers.event as ev
+from homeassistant.helpers.dispatcher import async_dispatcher_send
+from homeassistant.helpers.entity import DeviceInfo
+from datetime import timedelta
+from .gmc_connection import GMCConnection
+#from .skykettle import SkyKettle
+
+_LOGGER = logging.getLogger(__name__)
+
+PLATFORMS = [
+    Platform.SENSOR,
+    Platform.NUMBER,
+]
+
