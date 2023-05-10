3e71c1ebce1f55911dd30d9f47ec0a018742d50e
Sergey <rpmadm@yandex.ru>
2023-05-10 12:52:05 +0300

    update

--- custom_components/gmc300/sensor.py
+++ custom_components/gmc300/sensor.py
@@ -1,25 +1,43 @@
-from homeassistant.components.sensor import SensorEntity
+from __future__ import annotations
+
+from homeassistant.components.sensor import (
+    SensorDeviceClass,
+    SensorEntity,
+    SensorStateClass,
+)
+from homeassistant.const import TEMP_CELSIUS
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
+
+def setup_platform(
+    hass: HomeAssistant,
+    config: ConfigType,
+    add_entities: AddEntitiesCallback,
+    discovery_info: DiscoveryInfoType | None = None
+) -> None:
+    """Set up the sensor platform."""
+    add_entities([GMCSensor()])
 
 
 class GMCSensor(SensorEntity):
     _attr_has_entity_name = True
     _attr_icon = "mdi:radioactive"
+    _attr_native_unit_of_measurement = None
+    _attr_device_class = None
+    _attr_state_class = SensorStateClass.MEASUREMENT
 
-    def __init__(self):
-        self._is_on = False
-        self._attr_device_info = ...  # For automatic device registration
-        self._attr_unique_id = ...
+    #def __init__(self):
+    #    self._is_on = False
+    #    self._attr_device_info = ...  # For automatic device registration
+    #    self._attr_unique_id = ...
 
     @property
     def name(self):
         """Name of the entity."""
         return "GMC300"
     
-    @property
-    def native_value(self)
-        
-        return 378
-    
+
     async def _async_update_data(self):
-        self.native_value = 356
+        self._attr_native_value = 356
 
