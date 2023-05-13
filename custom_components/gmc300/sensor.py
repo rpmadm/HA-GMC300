7051eab56c8f57282f0749a3d0858db576fbcc1e
Sergey <rpmadm@yandex.ru>
2023-05-13 23:51:24 +0300

    update

--- custom_components/gmc300/sensor.py
+++ custom_components/gmc300/sensor.py
@@ -1,5 +1,5 @@
 import struct
-import serial
+#import serial
 
 from __future__ import annotations
 
@@ -43,12 +43,12 @@ class GMCSensor(SensorEntity):
     
 
     def update(self):
-    #    self._attr_native_value = self._attr_native_value + 1
-        s = serial.Serial( "/dev/ttyUSB-geiger", 57600 )
-        s.write(str.encode("<GETCPM>>"))
-        if s.inWaiting() > 0:
-           r = s.read(2)
-           r2 = struct.unpack('>H', r)[0]
-        return r2
+        self._attr_native_value = self._attr_native_value + 1
+    #    s = serial.Serial( "/dev/ttyUSB-geiger", 57600 )
+    #    s.write(str.encode("<GETCPM>>"))
+    #    if s.inWaiting() > 0:
+    #       r = s.read(2)
+    #       r2 = struct.unpack('>H', r)[0]
+    #    return r2
     #
     
