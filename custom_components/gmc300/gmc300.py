65b6fab76332e7a5d8529272f7661e3b78cf8cbf
Sergey <rpmadm@yandex.ru>
2023-05-17 21:56:42 +0300

    update

--- custom_components/gmc300/gmc300.py
+++ custom_components/gmc300/gmc300.py
@@ -59,16 +59,19 @@ def get_cpm(cpm_to_usievert=None):
 
     v_device.write(str.encode('<GETCPM>>'))
  #   if v_device.in_waiting > 0:
-    if v_device.inWaiting() > 0:
-        cpm = v_device.read(2)
+    while True 
+        if v_device.in_waiting() > 0:
+            cpm = v_device.read(2)
 
-        gmc_logger.debug('INFO: Сырое значение cpm ' + cpm)
+            gmc_logger.debug('INFO: Сырое значение cpm ' + cpm)
 
-        if cpm == '' or len(cpm) < 2:
-            gmc_logger.debug('WARNING: Нет корректного значения cpm')
-            return ''
-
-        value = struct.unpack(">H", cpm)[0]
+            if cpm == '' or len(cpm) < 2:
+                gmc_logger.debug('WARNING: Нет корректного значения cpm')
+                return ''
+            else:
+                value = struct.unpack(">H", cpm)[0]
+                gmc_logger.debug("INFO: Конечное значение value = " + str(value))
+                return value    
         time.sleep(2)
 #        unit_value = (value, 'CPM')
 #
@@ -79,8 +82,7 @@ def get_cpm(cpm_to_usievert=None):
 #            return '{:.4f} {:s}'.format(unit_value[0], unit_value[1])
 #        else:
 #            return '{:d} {:s}'.format(unit_value[0], unit_value[1])
-    gmc_logger.debug("INFO: Конечное значение value = " + str(value))
-    return value    
+    
 
 
 def convert_cpm_to_usievert(cpm, unit, cpm_to_usievert):
