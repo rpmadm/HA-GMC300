b998b2977537a055841cd0242b1cb0666bb15fd4
Sergey <rpmadm@yandex.ru>
2023-05-17 10:05:32 +0300

    update

--- custom_components/gmc300/sensor.py
+++ custom_components/gmc300/sensor.py
@@ -47,6 +47,7 @@ class GMCSensor(SensorEntity):
 
     def update(self):
     #    self._attr_native_value = self._attr_native_value + 1
+        r2 = none 
         s = serial.Serial("/dev/ttyUSB-geiger", 57600)
         s.write(str.encode("<GETCPM>>"))
         if s.inWaiting() > 0:
